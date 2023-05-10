30d2105916e5e548f47f4b8055a683ef847a5707
Sameer Brenn <sameer@hashflow.com>
2023-05-10 10:32:05 -0600

    blackify

--- validations.py
+++ validations.py
@@ -1,5 +1,8 @@
 import re
 
+
 def validate_maker_name(name: str) -> None:
-    if not re.match(r'^mm[0-9]+$', name):
-        raise ValueError(f"Maker name must be external name of format 'mm123'. Got '{name}'")
+    if not re.match(r"^mm[0-9]+$", name):
+        raise ValueError(
+            f"Maker name must be external name of format 'mm123'. Got '{name}'"
+        )
