4e3e1ae5f5a1c8a693c8e47b15987a004e3d88ac
Sameer Brenn <132616132+sameer-hashflow@users.noreply.github.com>
2023-05-12 14:39:46 -0600

    Move to the new async taker (#4)
    We need an async interface to the API because we need to make requests
    in parallel.
    
    Tested:
    
    ```
    ./qa.py --maker mm5 --chain 1 --env production
    QA testing maker 'mm5' against production on chain 1 with 30 requests/pair.
    
    Finding active makers ... done. mm5
    Fetching levels for mm5...done
    {'mm5': [{'baseToken': {'chainId': 1, 'address': '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599', 'name': 'WBTC', 'decimals': 8}, 'quoteToken': {'chainId': 1, 'address': 
    ...
    {'chainId': 1, 'address': '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', 'name': 'WETH', 'decimals': 18}, 'levels': [{'level': '1', 'price': '0.000557165311'}, {'level': '47775.903474', 'price': '0.000557165311'}]}]}
    ```

--- qa.py
+++ qa.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import argparse
+import asyncio
 import configparser
 from hashflow.helpers.validation import validate_evm_address, validate_chain_id
 from hashflow.api import HashflowApi
@@ -33,108 +34,108 @@ def get_args():
     return parser.parse_args()
 
 
-def handler(args, options):
+async def handler(args, options):
     validate_evm_address(options["qa_taker_address"])
     validate_maker_name(args.maker)
     validate_chain_id(args.chain)
 
-    api = HashflowApi("taker", "qa", options["auth_key"], args.env)
-    pair_provided = args.base_token is not None and args.quote_token is not None
+    async with HashflowApi("taker", "qa", options["auth_key"], args.env) as api:
+        pair_provided = args.base_token is not None and args.quote_token is not None
 
-    pair_str = f" for {args.base_token}-{args.quote_token}" if pair_provided else ""
-    more_str = f" with {args.num_requests} requests/pair" if args.num_requests else ""
-    if args.delay_ms:
-        more_str += f" and {args.delay_ms}ms delay"
+        pair_str = f" for {args.base_token}-{args.quote_token}" if pair_provided else ""
+        more_str = f" with {args.num_requests} requests/pair" if args.num_requests else ""
+        if args.delay_ms:
+            more_str += f" and {args.delay_ms}ms delay"
 
-    sys.stdout.write(
-        f"QA testing maker '{args.maker}' against {args.env} on chain {args.chain}{pair_str}{more_str}.\n\n"
-    )
-
-    sys.stdout.write("Finding active makers ... ")
-
-    try:
-        chain_makers = api.get_market_makers(
-            chain_id=args.chain, market_maker=args.maker
+        sys.stdout.write(
+            f"QA testing maker '{args.maker}' against {args.env} on chain {args.chain}{pair_str}{more_str}.\n\n"
         )
 
-        def check_maker(maker):
-            prefix = maker.split("_")[0]
-            return prefix == args.maker
+        sys.stdout.write("Finding active makers ... ")
 
-        makers = list(filter(check_maker, chain_makers))
-        if len(makers) == 0:
-            raise Exception(f"No makers available: {chain_makers}")
+        try:
+            chain_makers = await api.get_market_makers(
+                chain_id=args.chain, market_maker=args.maker
+            )
 
-    except Exception as e:
-        sys.stdout.write(f"Failed! {e}\n")
-        exit(-1)
+            def check_maker(maker):
+                prefix = maker.split("_")[0]
+                return prefix == args.maker
 
-    makers_list_or_one = makers if len(makers) > 1 else makers[0]
-    sys.stdout.write(f"done. {makers_list_or_one}\n")
-    sys.stdout.write(f"Fetching levels for {makers_list_or_one}...")
+            makers = list(filter(check_maker, chain_makers))
+            if len(makers) == 0:
+                raise Exception(f"No makers available: {chain_makers}")
 
-    try:
-        levels = api.get_price_levels(args.chain, makers)
-        if not levels.keys():
-            raise Exception("No maker levels.\n")
+        except Exception as e:
+            sys.stdout.write(f"Failed! {e}\n")
+            exit(-1)
 
-        def filter_fn(maker):
-            def filter_levels(entry):
-                pair = entry["pair"]
-                if pair_provided:
-                    pair_base_token_name = pair["baseTokenName"]
-                    pair_quote_token_name = pair["quoteTokenName"]
-                    if pair_provided and not (
-                        pair_base_token_name == args.base_token
-                        and pair_quote_token_name == args.quote_token
-                    ):
+        makers_list_or_one = makers if len(makers) > 1 else makers[0]
+        sys.stdout.write(f"done. {makers_list_or_one}\n")
+        sys.stdout.write(f"Fetching levels for {makers_list_or_one}...")
+
+        try:
+            levels = await api.get_price_levels(args.chain, makers)
+            if not levels.keys():
+                raise Exception("No maker levels.\n")
+
+            def filter_fn(maker):
+                def filter_levels(entry):
+                    pair = entry["pair"]
+                    if pair_provided:
+                        pair_base_token_name = pair["baseTokenName"]
+                        pair_quote_token_name = pair["quoteTokenName"]
+                        if pair_provided and not (
+                            pair_base_token_name == args.base_token
+                            and pair_quote_token_name == args.quote_token
+                        ):
+                            return False
+                    levels_data = entry["levels"]
+                    if not levels_data:
+                        sys.stdout.write(
+                            f" No levels for {maker} on {pair_base_token_name}-{pair_quote_token_name}. Continuing with next pair...\n"
+                        )
                         return False
-                levels_data = entry["levels"]
-                if not levels_data:
-                    sys.stdout.write(
-                        f" No levels for {maker} on {pair_base_token_name}-{pair_quote_token_name}. Continuing with next pair...\n"
-                    )
-                    return False
-                return True
+                    return True
 
-            return filter_levels
+                return filter_levels
 
-        def transform_levels(entry):
-            pair = entry["pair"]
-            pair_base_token_name = pair["baseTokenName"]
-            pair_quote_token_name = pair["quoteTokenName"]
-            base_token = {
-                "chainId": args.chain,
-                "address": pair["baseToken"],
-                "name": pair_base_token_name,
-                "decimals": pair["baseTokenDecimals"],
+            def transform_levels(entry):
+                pair = entry["pair"]
+                pair_base_token_name = pair["baseTokenName"]
+                pair_quote_token_name = pair["quoteTokenName"]
+                base_token = {
+                    "chainId": args.chain,
+                    "address": pair["baseToken"],
+                    "name": pair_base_token_name,
+                    "decimals": pair["baseTokenDecimals"],
+                }
+                quote_token = {
+                    "chainId": args.chain,
+                    "address": pair["quoteToken"],
+                    "name": pair_quote_token_name,
+                    "decimals": pair["quoteTokenDecimals"],
+                }
+                return {
+                    "baseToken": base_token,
+                    "quoteToken": quote_token,
+                    "levels": entry["levels"],
+                }
+
+            maker_levels = {
+                m: list(map(transform_levels, filter(filter_fn(m), l)))
+                for m, l in levels.items()
             }
-            quote_token = {
-                "chainId": args.chain,
-                "address": pair["quoteToken"],
-                "name": pair_quote_token_name,
-                "decimals": pair["quoteTokenDecimals"],
-            }
-            return {
-                "baseToken": base_token,
-                "quoteToken": quote_token,
-                "levels": entry["levels"],
-            }
+            sys.stdout.write("done\n")
+            print(maker_levels)
 
-        maker_levels = {
-            m: list(map(transform_levels, filter(filter_fn(m), l)))
-            for m, l in levels.items()
-        }
-        sys.stdout.write("done\n")
-        print(maker_levels)
-
-    except Exception as e:
-        sys.stdout.write(f"failed! {e}\n")
-        raise e
-        sys.exit(0)
+        except Exception as e:
+            sys.stdout.write(f"failed! {e}\n")
+            raise e
+            sys.exit(-1)
 
 
 if __name__ == "__main__":
     args = get_args()
     options = get_config(args.config)
-    handler(args, options)
+    asyncio.run(handler(args, options))
